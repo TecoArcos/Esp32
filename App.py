e2c30b72ca5802902c59f2516acef3732e2ae746
unknown <l18270085@tuxtla.tecnm.mx>
2022-05-27 13:06:36 -0500

    ver27052022

--- App.py
+++ App.py
@@ -1,23 +1,28 @@
-from  flask import  Flask, render_template
+
+from  flask import  Flask, render_template, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'ESP32'
 sql = MySQL(app)
 
-@app.route('/contac')
-def add_contac():
-    return 'asdasd'
+@app.route('/')
+def index():
+    return render_template('index.html')
+    
 
+@app.route('/add_contact', methods=['POST'])
+def add_contact():
+    if request.method == 'POST':
+       dato =  request.form['dato']
+       print(dato)
+       return dato
 
-
-@app.route('/edit')
-def edit_contac():
-    return '123456789'
-
+        
 @app.route('/delete')
 def delete_contac():
     return 'asdasd'
